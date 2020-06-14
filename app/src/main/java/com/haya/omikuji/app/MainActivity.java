54301e1c83f152b8abc4badbd23359e35535de52
Haya <max9173627@gmail.com>
2020-06-14 13:45:20 +0900

    [add]おみくじで大吉を出す

--- app/src/main/java/com/haya/omikuji/app/MainActivity.java
+++ app/src/main/java/com/haya/omikuji/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.haya.omikuji.app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         omikujibutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, "大吉", Toast.LENGTH_SHORT).show();
             }
         });
     }
