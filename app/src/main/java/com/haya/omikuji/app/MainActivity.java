a91fc6dd0a6dd2ecec4e8f259bbfadce308df68a
Haya <max9173627@gmail.com>
2020-06-14 15:31:43 +0900

    [fix]ランダム表示機能の修正

--- app/src/main/java/com/haya/omikuji/app/MainActivity.java
+++ app/src/main/java/com/haya/omikuji/app/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,7 +20,28 @@ public class MainActivity extends AppCompatActivity {
         omikujibutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "大吉", Toast.LENGTH_SHORT).show();
+                String unsei = "";
+                Random random = new Random();
+                switch (random.nextInt( 5)) {
+                    case 0:
+                        unsei = "大吉";
+                        break;
+                    case 1:
+                        unsei = "吉";
+                        break;
+                    case 2:
+                        unsei = "中吉";
+                        break;
+                    case 3:
+                        unsei = "小吉";
+                        break;
+                    case 4:
+                        unsei = "末吉";
+                        break;
+                    case 5:
+                        unsei = "凶";
+                }
+                Toast.makeText(MainActivity.this, unsei, Toast.LENGTH_SHORT).show();
             }
         });
     }
