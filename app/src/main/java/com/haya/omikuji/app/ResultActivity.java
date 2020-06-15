dc4e879a2ed78f2760b9cd7db7e95994d88e4bb5
Haya <max9173627@gmail.com>
2020-06-15 11:57:41 +0900

    [add]画面遷移後Viewを追加

---
+++ app/src/main/java/com/haya/omikuji/app/ResultActivity.java
@@ -0,0 +1,14 @@
+package com.haya.omikuji.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_main);
+    }
+}
