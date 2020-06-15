8d334b2fddac53a2f0cd6237d7713527a01731d4
Haya <max9173627@gmail.com>
2020-06-15 11:58:12 +0900

    [add]画面遷移した先におみくじの結果を表示　

--- app/src/main/java/com/haya/omikuji/app/MainActivity.java
+++ app/src/main/java/com/haya/omikuji/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.haya.omikuji.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,28 +21,12 @@ public class MainActivity extends AppCompatActivity {
         omikujibutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String unsei = "";
-                Random random = new Random();
-                switch (random.nextInt( 5)) {
-                    case 0:
-                        unsei = "大吉";
-                        break;
-                    case 1:
-                        unsei = "吉";
-                        break;
-                    case 2:
-                        unsei = "中吉";
-                        break;
-                    case 3:
-                        unsei = "小吉";
-                        break;
-                    case 4:
-                        unsei = "末吉";
-                        break;
-                    case 5:
-                        unsei = "凶";
-                }
-                Toast.makeText(MainActivity.this, unsei, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent();
+                intent.setClass( MainActivity.this,  ResultActivity.class);
+                startActivity(intent);
+
+
             }
         });
     }
--- app/src/main/java/com/haya/omikuji/app/ResultActivity.java
+++ app/src/main/java/com/haya/omikuji/app/ResultActivity.java
@@ -1,14 +1,45 @@
 package com.haya.omikuji.app;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_main);
+
+        TextView resultTextView = findViewById(R.id.result_text_view);
+
+        String unsei = "";
+        Random random = new Random();
+        switch (random.nextInt( 5)) {
+            case 0:
+                unsei = "大吉";
+                break;
+            case 1:
+                unsei = "吉";
+                break;
+            case 2:
+                unsei = "中吉";
+                break;
+            case 3:
+                unsei = "小吉";
+                break;
+            case 4:
+                unsei = "末吉";
+                break;
+            case 5:
+                unsei = "凶";
+        }
+
+        resultTextView.setText(unsei);
+//        Toast.makeText( ResultActivity.this, unsei, Toast.LENGTH_SHORT).show();
     }
 }
